ba75767b0b6d6829669174cec70e1db030a8511b
Kshitij Sharma <kshtjsharma68@gmail.com>
2022-07-14 21:34:19 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import SideBar from './components/Sidebar/SideBar';
+import RecommendedVideos from './components/recommended/RecommendedVideos';
+import SearchPage from './components/SearchPage/SearchPage';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+const Main = _ =>  (<div className="app__mainpage">
+<SideBar />
+<RecommendedVideos />
+</div>)
+
+const Search = _ => <div className="app__mainpage">
+<SideBar />
+<SearchPage />
+</div>;
+
+const VideoPage = _ => <Route path='/video/:videoId'>
+<div className="app__mainpage">
+  <VideoPlayer />
+</div>
+</Route>
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header />
+        <Routes>
+          <Route path='/video/:videoId' element={VideoPage} />
+          <Route path='/search/:searchQuery' element={<Search />}/>
+          <Route path='/' element={<Main />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
